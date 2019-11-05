7069493ed284532991b098aca03beb90e2250e32
apister <alexis.pister@inria.fr>
2019-11-05 17:43:30 +0100

    TP1 finished

--- TP1.c
+++ TP1.c
@@ -81,18 +81,78 @@ void effetMirroir(int x){
 }
 
 
+
+
+
+int string_length(char s[]) {
+   int c = 0;
+ 
+   while (s[c] != '\0')
+      c++;
+ 
+   return c;
+}
+
+int palindrome(char *word){
+
+    int len = string_length(word);
+
+    for (size_t i = 0; i < len-1; i++)
+    {
+        if (word[i] != word[len - i - 1]) {
+            return 0;
+        }
+    }
+    
+    return 1;
+}
+
+
+void pyramide(int n){
+    for (size_t i = 0; i < n; i++){
+        for (size_t j = 0; j < (n - i - 1); j++)
+        {
+            printf(" ");
+        }
+
+        for (size_t j = 0; j < (2*i) + 1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
+
+
 void main(int argc, char *argv[]){
     // exo1();
 
-    divisionEuclidienne(10, 2);
-    divisionEuclidienne(20, 7);
+    // divisionEuclidienne(10, 2);
+    // divisionEuclidienne(20, 7);
 
-    conversionTemp(3672);
+    // conversionTemp(3672);
 
-    int pow = puissance(2, 5);
-    printf("puissance : %d \n", pow);
+    // int pow = puissance(2, 5);
+    // printf("puissance : %d \n", pow);
 
-    effetMirroir(643);
+    // effetMirroir(643);
+
+    // char mot[3] = {'a', 'b', 'c'};
+    // int intab[3] = {1,2,3};
+    // printf("sizeoftab : %d , sizeofchar : %d", sizeof(mot), sizeof(char));
+
+    char chaine[] = "kayak";
+
+    int result = palindrome(chaine);
+    printf("palindrome result : %d \n", result);
+
+    int* adresse;
+
+
+
+    pyramide(8);
+
 }
 
 
